c32184859c91cd0526fe9d287a9cc055989fc081
dreamfunicular <toaburner@gmail.com>
2023-12-22 19:10:05 -0500

    API Code (Mostly) Finalized

--- api/backend.py
+++ api/backend.py
@@ -7,17 +7,68 @@ import time
 from openai import OpenAI
 from dotenv import load_dotenv
 
+def cleanupHTML(input):
+    output = ""
+    outputting = True
+
+    for c in input:
+        if c == '<' or c == '>':
+            outputting = not outputting
+        elif outputting:
+            output += c
+
+    output = output.rstrip()
+
+    return output
+
 
 def queryChatGPT(player, articleLink):
+    load_dotenv()
+    client = OpenAI(api_key=os.getenv("OPENAI_KEY"))
     articleText = ""
-    
 
-    prompt = "Yes or no: does the following text describe either that " + player + " has announced a definitive agreement to join a team, or that a team has announced a definitive agreement to sign " + player + "? Respond \"No\" if the news of the agreement is not from the team or player themselves, but rather from sources outside of the player or team.\n\n" + articleText
-    # response = client.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role":"user", prompt : prompt}])
-    # print(response["choices"][0]["message"]["content"])
+    espnAPIText = ""
+    for i in range(3):
+        espnAPIText = requests.get(articleLink)
+        if espnAPIText.status_code == 200:
+            break
+        if i == 2:
+            print("Error: Status code is", espnAPIText.status_code)
+            return "API Error"
+        
     
-    print(player, articleLink)
-    return "New York Mets"
+    articleJSON = espnAPIText.json()
+    articleText = articleJSON["headlines"][0]["story"]
+    articleText = cleanupHTML(articleText)
+
+    #  If so, respond simply with the full name of the team, stylized with the city name and nickname; for example, \"Washington Nationals\" or \"San Francisco Giants,\" with no other text in your response. Otherwise, respond simply with the word \"No.\"\
+
+    myPrompt = "Does the following text say with which team " + player.title() + " will certainly play in the next season? Ignore anything that the text says about any other player; focus solely on " + player.title() +".\n\n" + articleJSON["headlines"][0]["title"]  + "\n" + articleJSON["headlines"][0]["description"] + "\n" + articleText
+
+    # print(myPrompt)
+
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role":"user", "content" : myPrompt}], temperature=.3)
+    # print(response.choices[0].message.content)
+    responseText = response.choices[0].message.content
+
+    teamsFile = open("./teams.json", "r")
+    teams = json.load(teamsFile)
+    teamsFile.close()
+
+    for i in range(len(teams)):
+        team = teams[i][0]
+        if team in responseText:
+            if ("source" not in articleJSON["headlines"][0]["description"].lower()) and ("source" not in articleJSON["headlines"][0]["title"].lower()) and ("rumor" not in articleJSON["headlines"][0]["description"].lower()) and ("rumor" not in articleJSON["headlines"][0]["title"].lower()):
+                return team
+            else:
+                return "No."
+        
+    return "No."
+    
+    if (responseText.startswith("No")):
+        responseText = ""
+
+    return ""
 
 def updatePlayerStatusInFiles(player, newTeam):
     base=os.path.dirname(os.path.abspath("../html/index.html"))
@@ -43,7 +94,7 @@ def updatePlayerStatusInFiles(player, newTeam):
         playersFile.truncate()
         json.dump(players, playersFile)    
 
-def pullNews(watchedPlayers):
+def pullNews(watchedPlayers, readArticles):
     espnAPIText = ""
     for i in range(3):
         espnAPIText = requests.get("https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/news")
@@ -56,27 +107,36 @@ def pullNews(watchedPlayers):
     espnAPIDict = espnAPIText.json()
 
     for article in espnAPIDict["articles"]:
-        for player in watchedPlayers:
-            if player in article["description"]:
-                print("There's news on " + player + "!")
-                newTeam = queryChatGPT(player, article["links"]["api"]["news"]["href"])
-                
-                if newTeam != "":
-                    print(player + " is joining " + newTeam + "!")
-                    updatePlayerStatusInFiles(player, newTeam)
+        if not (article["dataSourceIdentifier"] in readArticles):
+            apiError = False
 
-"""while (True):
+
+            for player in watchedPlayers:
+                if ((player in article["description"].lower()) and (article["premium"] == False) and (article["type"] == "HeadlineNews") and ("source" not in article["description"].lower()) and ("source" not in article["headline"].lower()) and ("rumor" not in article["description"].lower()) and ("rumor" not in article["headline"].lower())):
+                    newTeam = queryChatGPT(player, article["links"]["api"]["news"]["href"])
+            
+                    if newTeam == "API Error":
+                        apiError = True
+                        break
+                    if newTeam != "":
+                        updatePlayerStatusInFiles(player, newTeam)
+            if apiError == False: 
+                with open("./readArticles.json", "r+") as readArticlesFile: 
+                    readArticlesJSON = json.load(readArticlesFile)
+                    readArticlesJSON.append(article["dataSourceIdentifier"])
+                    readArticlesFile.seek(0)
+                    readArticlesFile.truncate()
+                    json.dump(readArticlesJSON, readArticlesFile)
+
+while (True):
     watchedPlayersFile = open("./watchedPlayers.json", "r")
-    watchedPlayers = json.load(watchedPlayersFile)[
+    watchedPlayers = json.load(watchedPlayersFile)
     watchedPlayersFile.close()
 
-    pullNews(watchedPlayers)
+    readArticlesFile = open("./readArticles.json", "r")
+    readArticles = json.load(readArticlesFile)
+    readArticlesFile.close()
 
-    time.sleep(60)"""
+    pullNews(watchedPlayers, readArticles)
 
-load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_KEY"))
-print("Hello")
-response = client.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role":"user", "content" : "What were the primary causes of the American Revolution?"}])
-print(type(response))
-print(response.choices[0].message.content)
+    time.sleep(180)
